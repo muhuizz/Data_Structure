96d73e9b65bc0db9372dd8ecadc5824a54d04236
muhuizz <568772683@qq.com>
2016-09-21 13:10:33 +0800

    Vector

---
+++ Vector/test.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+#include "Vector.hpp"
+
+
+int main()
+{
+	TestVector();
+	system("pause");
+	return 0;
+}
