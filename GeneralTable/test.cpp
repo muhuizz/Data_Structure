bf0c2e515f6c13ddf28372ed968d7658ff4c6f00
muhuizz <568772683@qq.com>
2016-10-15 16:03:45 +0800

    test

---
+++ GeneralTable/test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include "GeneralTable.hpp"
+
+using namespace std;
+
+int main()
+{
+	TestGeneralizeTable();
+	system("pause");
+	return 0;
+}
