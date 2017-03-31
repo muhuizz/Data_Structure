648210a3996d4b769bf3fb3be4233ca51320334b
muhuizz <568772683@qq.com>
2017-03-31 22:49:19 +0800

    list

--- List/List.hpp
+++ List/List.hpp
@@ -1,238 +1,158 @@
-#pragma once
+#ifndef __LIST_HPP__
+#define __LIST_HPP__
+
 #include <iostream>
-#include <cassert>
 using namespace std;
 
 template <typename T>
 struct ListNode
 {
-public:
-	ListNode(const T& x)
-		:_data(x)
-		, _next(NULL)
-		, _prev(NULL)
-	{}
-public:
-	ListNode<T>* _next;
-	ListNode<T>* _prev;
+	ListNode* _next;
+	ListNode* _prev;
 	T _data;
-
+	ListNode(const T& x=T())
+		:_next(NULL)
+		 ,_prev(NULL)
+		 ,_data(x)
+	{}
 };
 
-//
-//STL----->复用+效率
-//
-template<class T, class Ref, class Ptr>
+template <typename T, typename Ref, typename Ptr>
 struct ListIterator
 {
-	typedef ListIterator<T, Ref, Ptr> Self;// 重定义寄存器
-	typedef ListNode<T> Node;			//重定义结点
+	typedef ListNode<T> Node;
+	typedef ListIterator<T, Ref, Ptr> Self;
 public:
 	ListIterator(Node* node)
 		:_node(node)
 	{}
-	ListIterator()
-	{}
-	ConstIterator(Node* node)
-		:_node(node)
-	{}
-	ConstIterator()
-	{}
-	Ref operator*()
-	{
-		return _node->_data;
-	}
-	Ptr operator->()
-	{
-		//return &(_node->_data);
-		return &(operator*());
-	}
 
-	bool operator==(const Self& it) const
+	bool operator==(const Self& it)
 	{
-		return _node == it._node;
+		return (it._node == _node);
 	}
-
-	bool operator!=(const Self& it) const
+	bool operator!=(const Self& it)
 	{
-		return _node != it._node;
+		return (it._node != _node);
 	}
-
-	Self& operator++()
-	{
-		//return ListIterator(_node->_next);
-		_node = _node->_next;
-		return *this;
-	}
-
-	Self operator++(int)
+	Self operator++(int)	// hou zhi 
 	{
 		Self tmp(_node);
 		_node = _node->_next;
 		return tmp;
 	}
-
-	Self& operator--()
+	Self& operator++()
 	{
-		//retrun ListIterator(_node->_prev);
-		_node = _node->_prev;
-		return *this;
+		_node=_node->_next;
+		return _node;
 	}
-
 	Self operator--(int)
 	{
-		Self tmp(_node);
-		_node = _node->_prev;
-		retrun tmp;
+		Node tmp(_node);
+		_node=_node->_prev;
+		return tmp;
 	}
+	Self& operator--()
+	{
+		_node=_node->_prev;
+		return _node;
+	}
+	
+	T& operator*()
+	{
+		return _node->_data;
+	}
+
+	Ptr operator->()
+	{
+		return &(operator*());
+	}
+
 public:
 	Node* _node;
 };
 
-template<typename T>
+template <typename T>
 class List
 {
-public:
-
 	typedef ListNode<T> Node;
-	typedef ListIterator<T, T&, T*> Iterator;// 重定义寄存器
-	typedef ListIterator<T, const T&, const T*> ConstIterator;//重定义const类型的迭代器
-	//两个模板参数方便实现
-
+public:
+	typedef ListIterator<T, T&, T*> Iterator;
+	typedef ListIterator<const T, const T&, const T*> ConstIterator;
 	List()
 	{
-		_head = BuyNode(T());
+		_head = _buyNode(T());
 		_head->_next = _head;
 		_head->_prev = _head;
 	}
 
 	Iterator Begin()
 	{
-		//return Iterator(_head->_next);
-		return _head->_next;
+		return Iterator(_head->_next);
 	}
 	Iterator End()
 	{
-		//return Iterator(_head);
-		return _head;
-	}
-	//const对象调用的
-	ConstIterator Begin() const
-	{
-		return Iterator(_head->_next);
-	}
-	ConstIterator End() const
-	{
 		return Iterator(_head);
-		//return _head;//类型强转 explicit
 	}
-	void Insert(const Iterator& it, const T& x)
+	void Insert(Iterator pos, const T& x)
 	{
-		Node* front = it._node->_prev;
-		Node* node = BuyNode(x);
-
-		node->_next = it._node;
-		node->_prev = front;
-		front->_next = node;
-		it._node->_prev = node;
+		Node* tmp = _buyNode(x);
+		Node* prev = pos._node->_prev;
+		prev->_next = tmp;
+		tmp->_prev = prev;
+		tmp->_next = pos._node;
+		pos._node->_prev = tmp;
 	}
-	Iterator Erase(Iterator it)
+	void Erase(Iterator pos)
 	{
-		assert(it != End());
-		Node* front = it._node->_prev;
-		Node* next = it._node->_next;
-		front->_next = next;
-		next->_prev = front;
-		delete Iterator(it._node);
+		if(pos == End())
+			return;
+		Node* prev = pos._node->_prev;
+		Node* next = pos._node->_next;
+		prev->_next = next;
+		next->_prev = prev;
+		_destory(pos._node);
 	}
 	void PushBack(const T& x)
 	{
-		//Insert(_head->_prev, x);
 		Insert(End(), x);
 	}
 	void PopBack()
 	{
-		assert(_head != _head->_next);
-		Erase(Iterator(_head->_prev));
+		if(_head->_next != _head)
+		{
+			Erase(Iterator(_head->_prev));
+		}
 	}
 	void PushFront(const T& x)
 	{
 		Insert(Begin(), x);
 	}
+
 	void PopFront()
 	{
-		assert(_head != _head->_next);
-		Erase(Begin());
+		if(_head->_next != _head)
+		{
+			Erase(Begin());
+		}
 	}
-	void PrintList()
-	{
-		List<T>::Iterator it1 = 
-	}
-private:
-	Node* BuyNode(const T& x)
+protected:
+	Node* _buyNode(const T& x)
 	{
 		Node* tmp = new Node(x);
 		return tmp;
 	}
-private:
-	Node* _head;
+
+	void _destory(Node* node)
+	{
+		if(node != NULL)
+		{
+			delete node;
+			node = NULL;
+		}
+	}
+protected:
+	Node* _head;	// shao bin
 };
 
-struct A
-{
-	int _i;
-	char _c;
-	A(int i = 10,char c = 'a')
-		:_i(i)
-		, _c(c)
-	{}
-};
-
-void TestList()
-{
-	List<int> li;
-	//li.PushBack(1);
-	//li.PushBack(2);
-	//li.PushBack(3);
-	//li.PushBack(4);
-	//li.PushBack(5);
-	li.PushFront(1);
-	li.PushFront(2);
-	li.PushFront(3);
-	li.PushFront(4);
-	li.PushFront(5);
-	//li.PopFront();
-	//li.PopFront();
-
-	//li.PopBack();
-	//li.PopBack();
-	//li.PopBack();
-	//li.PopBack();
-	//li.PopBack();
-	//li.PopBack();
-
-	List<int>::Iterator it = li.Begin();
-	while (it != li.End())
-	{
-		cout << *it<< " ";
-		it++;
-	}
-	cout << endl;
-}
-
-void TestList2()
-{
-	List<A> li;
-	li.PushBack(A());
-	li.PushBack(A());
-	li.PushBack(A());
-	li.PushBack(A());
-	li.PushBack(A());
-
-	List<A>::Iterator it = li.Begin();
-	for (; it != li.End(); it++)
-	{
-		cout << it->_c << endl;
-	}
-
-}
+#endif// __LIST_HPP__
--- List/test.cpp
+++ List/test.cpp
@@ -1,11 +1,56 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
 #include "List.hpp"
-#include <cstdlib>
+
+struct A
+{
+	int a;
+	char c;
+	A()
+		:a(1)
+		 ,c('c')
+	{}
+};
+
 int main()
 {
-	//TestList();
-	TestList2();
-	system("pause");
+	List<A> l2;
+	l2.PushBack(A());
+	List<A>::Iterator it = l2.Begin();
+	cout<<it->a<<endl;
+	cout<<it->c<<endl;
+
+
+//	List<int> l1;
+//	l1.PushFront(1);
+//	l1.PushFront(2);
+//	l1.PushFront(3);
+//	l1.PushFront(4);
+////	l1.PushBack(1);
+////	l1.PushBack(2);
+////	l1.PushBack(3);
+////	l1.PushBack(4);
+//	List<int>::Iterator it = l1.Begin();
+//	while(it != l1.End())
+//	{
+//		cout<<*it<<endl;
+//		it++;
+//	}
+//	cout<<endl;
+//	l1.PopFront();
+//	l1.PopFront();
+//	l1.PopFront();
+//	l1.PopFront();
+//	l1.PopFront();
+//	l1.PopFront();
+////	l1.PopBack();
+////	l1.PopBack();
+////	l1.PopBack();
+////	l1.PopBack();
+////	l1.PopBack();
+////	l1.PopBack();
+//	while(it != l1.End())
+//	{
+//		cout<<*it<<endl;
+//		it++;
+//	}
 	return 0;
 }
